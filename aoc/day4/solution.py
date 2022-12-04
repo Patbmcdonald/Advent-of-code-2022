f1f22876e7aa012d24c37a4a02c2b4982a75d6ff
Patrick McDonald <pat.b.mcdonald@gmail.com>
2022-12-04 00:26:46 -0500

    Day 4 solution

--- aoc/day4/solution.py
+++ aoc/day4/solution.py
@@ -6,14 +6,52 @@ class DayFourSolution(BaseSolution):
         return "day4"
     
     def part_one(self):
-        return 0
         
+        counter = 0
+        for line in self._get_puzzle_data():
+            pairings = line.split(",")
+            
+            first = self._fill_range(pairings[0])
+            second = self._fill_range(pairings[1])
+
+            # Get Union
+            overlaps = first & second
+            
+            # If Overlaps equals either first or second, then we consumed one totally.
+            if overlaps == first or overlaps == second:
+                counter += 1
+            
+        return counter
+        
+    def _fill_range(self, pair):
+        """ Fill in elfs based on range """
+        pairing = pair.split("-")
+        start = int(pairing[0])
+        end = int(pairing[1])
+        overlaps = set()
+        for i in range(start, end + 1):
+            overlaps.add(i)
+            
+        return overlaps
+      
     def part_two(self):
-        return 0
+        counter = 0
+        for line in self._get_puzzle_data():
+            pairings = line.split(",")
+            
+            first = self._fill_range(pairings[0])
+            second = self._fill_range(pairings[1])
+
+            overlaps = first & second
+            
+            if len(overlaps) > 0:
+                counter += 1
+            
+        return counter
     
     def test_part_one(self):
-        return self.part_one() == 0
+        return self.part_one() == 2
         
     def test_part_two(self):
-        return self.part_two() == 0
+        return self.part_two() == 4
         
